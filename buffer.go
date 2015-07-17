cad93807c197cbe490ad55c5ce5b3b37c377e085
Chris Lunsford <cllunsford@gmail.com>
2015-07-17 06:38:05 -0400

    Applying go fmt

--- buffer.go
+++ buffer.go
@@ -1,8 +1,8 @@
 package main
 
 import (
-	"io"
 	"bytes"
+	"io"
 )
 
 // implements io.Writer to receive streaming logs
--- main_test.go
+++ main_test.go
@@ -3,8 +3,8 @@ package main
 import (
 	"testing"
 
-	"github.com/stretchr/testify/assert"
 	"github.com/fsouza/go-dockerclient"
+	"github.com/stretchr/testify/assert"
 )
 
 func TestPruneChanges(t *testing.T) {
